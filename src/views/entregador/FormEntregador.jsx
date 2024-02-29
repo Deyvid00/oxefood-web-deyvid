5b35bc623972b638b55468ee022896de44955fc5
aluno <aluno@ifpe>
2024-02-29 17:13:52 -0300

    first commit

---
+++ src/views/entregador/FormEntregador.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import InputMask from 'react-input-mask';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
+export default function FormEntregador () {
+
+    return (
+
+        <div>
+
+            <div style={{marginTop: '3%'}}>
+
+                <Container textAlign='justified' >
+
+                    <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+                    <Divider />
+
+                    <div style={{marginTop: '4%'}}>
+
+                        <Form>
+
+                            <Form.Group widths='equal'>
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='titulo'
+                                    maxLength="100"
+                                />
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='codigo do produto'>
+                                    <InputMask
+                                        required
+                                        mask="999.999.999-99"
+                                    /> 
+                                </Form.Input>
+
+                            </Form.Group>
+                            
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    label='Descrição'
+                                    width={6}>
+                                    <InputMask 
+                                        mask=""
+                                    /> 
+                                </Form.Input>
+
+                                
+
+                                <Form.Input
+                                    fluid
+                                    label='valor do usuario'
+                                    width={6}
+                                >
+                                    <InputMask 
+                                        mask="999999999" 
+                                        maskChar={null}
+                                        placeholder="Ex: 20/03/1985"
+                                    /> 
+                                </Form.Input>
+
+                            </Form.Group>
+                        
+                        </Form>
+                        
+                        <div style={{marginTop: '4%'}}>
+
+                            <Button
+                                type="button"
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='orange'
+                            >
+                                <Icon name='reply' />
+                                Voltar
+                            </Button>
+                                
+                            <Button
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
+
+                        </div>
+
+                    </div>
+                    
+                </Container>
+            </div>
+        </div>
+
+    );
+
+}
